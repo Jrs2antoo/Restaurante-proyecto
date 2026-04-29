d9166269128bb0ba0a5d004c864a747e58be3f15
Juan Santana Rosales <juanilayala2005@gmail.com>
2026-04-29 09:06:25 +0200

    router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { initializeApp } from "firebase/app";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQMuMbd-L33zJnhF6zcMAwSAWe508QDm4",
+  authDomain: "restarurante-la-brasa.firebaseapp.com",
+  projectId: "restarurante-la-brasa",
+  storageBucket: "restarurante-la-brasa.firebasestorage.app",
+  messagingSenderId: "942336675654",
+  appId: "1:942336675654:web:485640e7191bad5e1d1f47",
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ frontend/src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import HomePage from "@/components/HomePage.vue";
+import CatalogoPage from "@/components/CatalogoPage.vue";
+import LoginPage from "@/components/LoginPage.vue";
+import RegisterPage from "@/components/RegisterPage.vue";
+import AdministracionPage from "@/components/AdministracionPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: HomePage,
+      meta: { requireAuth: false, onlyAdmin: false },
+    },
+    {
+      path: "/login",
+      component: LoginPage,
+      meta: { requireAuth: false, onlyAdmin: false },
+    },
+    {
+      path: "/register",
+      component: RegisterPage,
+      meta: { requireAuth: false, onlyAdmin: false },
+    },
+    {
+      path: "/catalogo",
+      component: CatalogoPage,
+      meta: { requireAuth: true, onlyAdmin: false },
+    },
+    {
+      path: "/administracion",
+      component: AdministracionPage,
+      meta: { requireAuth: true, onlyAdmin: true },
+    },
+  ],
+});
+
+const esperarUsuario = () => {
+  return new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      unsubscribe(); // Deja de escuchar en cuanto recibe la primera respuesta
+      resolve(user);
+    });
+  });
+};
+
+router.beforeEach(async (to, from) => {
+  const usuarioLogueado = await esperarUsuario();
+  const ADMIN_ID = "jPW14sWmJpNMeVhaJURSIbLEVj72";
+
+  if (to.meta.onlyAdmin && usuarioLogueado?.uid !== ADMIN_ID) {
+    return "/inicio"; // Redirige a la página de administración si el usuario no es admin
+  }
+
+  if (to.meta.requireAuth && !usuarioLogueado) {
+    return "/login";
+  }
+
+  if (!to.meta.requireAuth && usuarioLogueado) {
+    return "/inicio"; // Redirige a la página de inicio si el usuario ya está logueado
+  }
+});
+export default router;
